755827bbaeb8be34f7941ef6e1c0885352949ec1
k1ngS <marcos.beltrao@outlook.pt>
2025-08-05 22:45:53 -0300

    feat: update TRPC integration to use createTRPCReact and enhance client setup

--- apps/web/src/utils/trpc.ts
+++ apps/web/src/utils/trpc.ts
@@ -1,6 +1,19 @@
-import { createTRPCContext } from "@trpc/tanstack-react-query";
+import { httpLink } from "@trpc/client";
+import { createTRPCReact } from "@trpc/react-query";
 import type { AppRouter } from "../../../server/src/routers";
 
-export const { TRPCProvider, useTRPC, useTRPCClient } =
-  createTRPCContext<AppRouter>();
+export const trpc = createTRPCReact<AppRouter>();
 
+export const trpcClient = trpc.createClient({
+	links: [
+		httpLink({
+			url: `${import.meta.env.VITE_SERVER_URL}/trpc`,
+			fetch(url, options) {
+				return fetch(url, {
+					...options,
+					credentials: "include",
+				});
+			},
+		}),
+	],
+});
